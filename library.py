75ffd5f338a456d1fbde38774ed3cfc5e8f8f0bb
RSingh118 <107784657+RSingh118@users.noreply.github.com>
2022-12-12 09:33:19 -0800

    Add files via upload

---
+++ library.py
@@ -0,0 +1,11 @@
+# Rudransh Singh
+# SOURCES:
+#https://www.youtube.com/watch?v=KUFmCwCVXWs&list=LL&index=45&t=464s
+#https://neptune.ai/blog/predicting-stock-prices-using-machine-learning
+#https://www.youtube.com/watch?v=1O_BenficgE&list=LL&index=8&t=366
+
+import yfinance as yf
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import sklearn as sks
